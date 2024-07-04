311971c4ccf4755d9a6e4288f4dfe9d19a6431bc
Wilson Reyhan <jwreyhan@gmail.com>
2024-07-03 22:08:16 -0500

    "Fixed dashboard/page bug"

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,11 +1,6 @@
 import { TaskContainer } from "../ui/taskcontainer";
 
 export default function Page() {
-/* tasks: {
-    title: string;
-    dueDate: string;
-    project: string;
-  }[] */
   const tasks = [
     { title: "To Do's", dueDate: "July 5", project: "Stersonal" },
     { title: "To Do's #2", dueDate: "July 6", project: "Stersonality" },
@@ -14,13 +9,13 @@ export default function Page() {
   return (
     <>
       {tasks.map((task, index) => (
-        <TaskContainer
-          className="pt-2"
-          title={task.title}
-          dueDate={task.dueDate}
-          project={task.project}
-          key={index}
-        />
+        <div className="pt-2" key={index}>
+          <TaskContainer
+            title={task.title}
+            dueDate={task.dueDate}
+            project={task.project}
+          />
+        </div>
       ))}
     </>
   );
